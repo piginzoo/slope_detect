66ed510e4778e67727b441b5dcf5ead1b059fb36
piginzoo <piginzoo@gmail.com>
2019-05-06 19:09:26 +0800

    recreate backgrounds

--- bin/imgen.sh
+++ bin/imgen.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "" ]; then
-    echo "Usage: imgen.sh  <dir:data/raw> <type:train|test|validate>"
+    echo "Usage: imgen.sh  <dir:data/raw> <type:train|test|validate> <background dir>"
     echo "dir:原始图像所在"
     exit
 fi
 
-python data_generator/rotate.py --dir $1 --type $2
+python data_generator/rotate.py --dir $1 --type $2 --background $3
--- data_generator/rotate.py
+++ data_generator/rotate.py
@@ -4,15 +4,30 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter,ImageOps
 import random
 import os
 logger = logging.getLogger("rotate")
-ROTATE_ANGLE = 30        # 随机旋转角度
+ROTATE_ANGLE = 5        # 随机旋转角度
+NUM_IN_DIRECTION = 2     # 每个方向上的数量
 classes_name = ["正图","90度","180度","270度"]
 degrees = [0,90,180,270]
+
 
 def init_logger():
     logging.basicConfig(
         format='%(asctime)s : %(levelname)s : %(message)s',
         level=logging.DEBUG,
         handlers=[logging.StreamHandler()])
+
+
+def rotate_in_background(img,angle):
+    global bg_list
+    back_img = random.choice(bg_list)
+    img = img.convert('RGBA')
+    img = img.rotate(angle,expand = 1)
+    # fff = Image.new('RGBA',img.size,(255,)*4)
+    # img = Image.composite(img,fff,img)
+    # img = img.crop((0, 0, img.size[0], img.size[1]))
+    back_img = back_img.crop((0, 0, img.size[0], img.size[1]))
+    back_img.paste(img,(0,0),img)
+    return back_img
 
 
 def degree_rotate(img,degree):
@@ -22,6 +37,24 @@ def degree_rotate(img,degree):
     center = (w // 2, h // 2)
     logger.debug("指定角度旋转度数:%f" % degree)
     return img.rotate(degree,center=center,expand=1)
+
+
+# 生成一张图片, 1200x1920
+def load_all_backgroud_images(bground_path):
+    bground_list = []
+
+    for img_name in os.listdir(bground_path):
+        image = Image.open( os.path.join(bground_path , img_name))
+        if image.mode == "L":
+            logger.error("图像[%s]是灰度的，转RGB",img_name)
+            image = image.convert("RGB")
+
+        bground_list.append(image)
+        logger.debug("加载背景图片：%s",bground_path + img_name)
+    logger.debug("所有图片加载完毕")
+
+    return bground_list
+
 
 
 def process_folder(dir,type):
@@ -60,13 +93,21 @@ def rotate_one_image(raw_image_name,label_file,target_dir):
 
 # 旋转一个角度，每一个角度生成5~10个样本并存储
 def rotate_one_direction(image,image_name,subfix,label_file,target_dir,clazz):
-    random_num = random.randint(5,10)  # 5~10的随机数
+    random_num = NUM_IN_DIRECTION #random.randint(5,10)  # 5~10的随机数
+
+    # 生成旋转角度，最后需要一张旋转角度为0的
+    degrees = []
     for i in range(1, random_num):
+        small_degree = random.uniform(-ROTATE_ANGLE, ROTATE_ANGLE)  # 随机旋转0-30度
+        degrees.append(small_degree)
+    degrees.append(0)
+
+    i = 0
+    for degree in degrees:
         name = image_name+"_"+str(i)
         rotated_file_name = os.path.join(target_dir,name+subfix)
-
-        small_degree = random.uniform(-ROTATE_ANGLE, ROTATE_ANGLE)  # 随机旋转0-30度
-        rotated_file = degree_rotate(image,small_degree)
+        # rotated_file = degree_rotate(image,degree)
+        rotated_file = rotate_in_background(image,degree)
         try:
             rotated_file.save(rotated_file_name)
         except IOError as e:
@@ -79,6 +120,7 @@ def rotate_one_direction(image,image_name,subfix,label_file,target_dir,clazz):
         label_file.write("\n")
 
         logger.debug("保存旋转后的图片&标签：%s", rotated_file_name)
+        i+= 1
 
 
 # 把指定目录下的文件都旋转，生成测试和验证代码
@@ -89,13 +131,18 @@ if __name__ == '__main__':
     init_logger()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--type") # 啥类型的数据啊，train/validate/test
-    parser.add_argument("--dir")  # 这个程序的主目录
+    parser.add_argument("--type")       # 啥类型的数据啊，train/validate/test
+    parser.add_argument("--dir")        # 这个程序的主目录
+    parser.add_argument("--background") # 背景图的目录
     args = parser.parse_args()
     DATA_DIR = args.dir
     TYPE= args.type
 
     images_dir = os.path.join("data",TYPE)
     if not os.path.exists(images_dir): os.makedirs(images_dir)
+
+    global bg_list
+    bg_list = load_all_backgroud_images(args.background)
+
 
     process_folder(DATA_DIR, TYPE)
