b5588c81f63ef3e67949b234f1dd1e4d8abced67
piginzoo <piginzoo@gmail.com>
2019-05-07 20:12:45 +0800

    revise startup script

--- bin/web.sh
+++ bin/web.sh
@@ -1,19 +1,18 @@
 Date=$(date +%Y%m%d%H%M)
 
 if [ "$1" = "stop" ]; then
-    echo "停止训练"
-    ps aux|grep python|grep slope_detect|awk '{print $2}'|xargs kill -9
+    echo "停止倾斜探测服务器"
+    ps aux|grep python|grep slope_detect_server|awk '{print $2}'|xargs kill -9
     exit
 fi
 
-
-nohup \
 name=slope_detect \
 model_dir=model \
 model_file=ctpn-2019-05-07-14-19-35-201.ckpt \
 CUDA_VISIBLE_DEVICES=0 \
+nohup \
     gunicorn \
-    web.server:app \
+    web.slope_detect_server:app \
     --workers=1 \
     --worker-class=gevent \
     --bind=0.0.0.0:8080 \
