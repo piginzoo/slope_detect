38842183df16d2dc9a735e61ca7806a0c0dfa38b
piginzoo <piginzoo@gmail.com>
2019-05-07 18:26:39 +0800

    minor bugfix

--- bin/web.sh
+++ bin/web.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 gunicorn --workers=1 --worker-class=gevent web.server:app --bind=0.0.0.0:8080 --timeout=300
+CUDA_VISIBLE_DEVICES=0 gunicorn \
+    web.server:app \
+    --workers=1 \
+    --worker-class=gevent \
+    --bind=0.0.0.0:8080 \
+    --timeout=300 \
+    --model_dir=model \
+    --model_file=ctpn-2019-05-07-14-19-35-201.ckpt
