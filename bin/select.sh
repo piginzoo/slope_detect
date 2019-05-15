2ae9a705ed013b896cfdf030b137e971ab1b560a
piginzoo <piginzoo@gmail.com>
2019-05-15 17:10:11 +0800

    implement select feature

--- bin/select.sh
+++ bin/select.sh
@@ -3,21 +3,11 @@ if [ "$1" = "" ]; then
     exit
 fi
 
-if [ "$1" = "help" ]; then
-    echo "select.sh 使用说明：
-    --image_dir     被预测的图片目录
-    --pred_dir      预测后的结果的输出目录
-    --model_dir     model的存放目录，会自动加载最新的那个模型
-    --model_file    model的模型文件
-    --target_dir    斜图片的备份目录"
-    exit
-fi
-
 echo "开始检测图片的倾斜....."
 
-python main/select.py \
-    --image_name=$1 \
+CUDA_VISIBLE_DEVICES=0 python main/select.py \
     --pred_dir=data/pred \
     --debug=True \
     --target=$2 \
-    --model_dir=model
+    --model_dir=model \
+    --model_file=ctpn-2019-05-07-14-19-35-201.ckpt
