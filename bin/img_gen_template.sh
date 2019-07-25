0b9208ce4e0dfa1859d219343c2f04eb8d2f0238
hongyanyang1 <hongyanyang1@creditease.cn>
2019-07-25 15:51:17 +0800

    修改生成模板图片代码和脚本

--- bin/img_gen_template.sh
+++ bin/img_gen_template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ "$1" = "" ]; then
-    echo "dir:工程根目录 -> project path  sum:使用主图像个数"
+    echo "dir:数据根目录  sum:主图像个数 repeat:重复执行次数，默认1"
     exit
 fi
 
-python data_generator/gen_img_template.py --dir $1 --sum $2
+python data_generator/gen_img_template.py --dir $1 --sum $2 --repeat $3
--- data_generator/gen_img_template.py
+++ data_generator/gen_img_template.py
@@ -5,14 +5,7 @@ import os
 import random
 
 import cv2 as cv
-import keras
 import numpy as np
-import tensorflow as tf
-
-print("tensorflow", tf.__version__)
-print("keras", keras.__version__)
-print("cv", cv.__version__)
-print("np", np.__version__)
 
 # 常量
 global ROOT
@@ -31,15 +24,25 @@ def filter_file_subfix(fileSubfix):
 
 
 # 加载所有背景图
-def load_all_backgroud_images(bground_path):
+def loadAllBackgroudImages(bgroundPath, maxSize=-1, imagePathName=None):
     bground_list = []
-    for img_name in os.listdir(bground_path):
+
+    if imagePathName != None:
+        image = cv.imread(imagePathName)
+        bground_list.append(image)
+        return bground_list
+
+    idx = 0
+    for img_name in os.listdir(bgroundPath):
         (filepath, fileName) = os.path.split(img_name)
         name, subfix = os.path.splitext(fileName)
         if filter_file_subfix(subfix):
-            image = cv.imread(bground_path + img_name)
+            image = cv.imread(bgroundPath + img_name)
             bground_list.append(image)
-            print("    加载背景图片：", bground_path + img_name)
+            print("    加载背景图片：", bgroundPath + img_name)
+        idx += 1
+        if idx > maxSize > 0:
+            break
 
     print("背景总共数量", len(bground_list))
     return bground_list
@@ -55,7 +58,7 @@ angleList = {
 
 
 # 按分类获取一个随机角度
-def get_angle(idx):
+def getAngle(idx):
     al = angleList[idx]
     angle = random.randint(-5, 5)
     angle = angle + al
@@ -64,15 +67,20 @@ def get_angle(idx):
 
 # print(random_angle(1)),exit(0)
 
+# 得到一个随机标记 True or False
+def getRandomFlag():
+    return random.choice([True, False])
+
+
 # 获取一个随机的宽高
-def get_random_wh():
-    w = random.randint(20, 100)
-    h = random.randint(20, 100)
+def getRandomWH():
+    w = random.randint(0, 200)
+    h = random.randint(0, 200)
     return w, h
 
 
 # 获取一个仿射数值
-def get_random_affine_offset(angle, rw, rh):
+def getRandomAffineOffset(angle, rw, rh):
     """
     根据角度获取一个合适的随机投射数值
     :return 类型 0上 1左 2下 3右 ,x,y
@@ -80,7 +88,7 @@ def get_random_affine_offset(angle, rw, rh):
     direction = 0
     if angle <= 1:
         direction = random.randint(0, 1)
-    elif angle > 1 and angle <= 3:
+    elif 1 < angle <= 3:
         direction = random.randint(2, 3)
 
     x = random.randint(0, rw)
@@ -110,7 +118,7 @@ def get_random_affine_offset(angle, rw, rh):
 
 # 根据主图宽高类型，随机获取一个背景图，直到获取同样类型的背景图为址
 # 最多尝试获取次数为 c < 10
-def get_random_bg_img(mainimg, c):
+def getRandomBGImg(mainimg, c):
     """
     根据主图类型获取背景图
     :param mainimg:主图像
@@ -143,13 +151,13 @@ def get_random_bg_img(mainimg, c):
         return ranBgimg
     else:
         # 两个图类型不一样，重新获取背景图
-        return get_random_bg_img(mainimg, c + 1)
+        return getRandomBGImg(mainimg, c + 1)
 
 
 # print(get_random_bg_img(Image.open("images/20190402051418692hjhjiuu.jpg"), 0)), exit(0)
 
 # 旋转图片，计算旋转后的大小
-def rotate_bound(image, angle):
+def rotateBound(image, angle):
     # grab the dimensions of the image and then determine the
     # center
     (h, w) = image.shape[:2]
@@ -181,44 +189,44 @@ def rotate_bound(image, angle):
 
 
 # 旋转图片并投射图片
-def gen_rotate_img(mainimg, fileName, label_file):
+def genRotateImg(mainimg, labelFile, fileName, batchNum):
     for idx in range(0, len(angleList)):  # range(0, len(angleList)):
-        angle = get_angle(idx)
+        angle = getAngle(idx)
         startTime = datetime.datetime.now()
         # rotateImg = mainimg.rotate(angle, expand=True)
         # mw, mh = rotateImg.size
-        rotateImg = rotate_bound(mainimg, angle)
+        rotateImg = rotateBound(mainimg, angle)
         (mw, mh) = rotateImg.shape[:2]
 
         print("旋转主图:", mw, mh, angle, "用时:", (datetime.datetime.now() - startTime).seconds)
         # 随机加载一个背景图
         startTime = datetime.datetime.now()
-        ranBgimg = get_random_bg_img(rotateImg, 0)
+        randomBackgroundImg = getRandomBGImg(rotateImg, 0)
         print("随机加载一个背景图用时:", (datetime.datetime.now() - startTime).seconds)
         # 生成仿射图片
-        gen_affine_img(ranBgimg, rotateImg, idx, label_file, fileName)
+        genAffineImg(randomBackgroundImg, rotateImg, idx, labelFile, fileName, batchNum)
 
 
 # 生成仿射图片
-def gen_affine_img(bgimg, mainimg, angle_type, label_file, fileName=None):
+def genAffineImg(bgimg, mainimg, angleType, labelFile, fileName, batchNum):
     """
     1.将角度图片生成仿射图片并做背景透明处理
     2.将生成好的仿射图片与背景图合并
 
     :param bgimg: 背景图
     :param mainimg:主图
-    :param angle_type:角度类型 上0 左1 下2 右3
+    :param angleType:角度类型 上0 左1 下2 右3
     :return:
     """
     startTime = datetime.datetime.now()
     # 获取一个随机的xy
-    rw, rh = get_random_wh()
+    rw, rh = getRandomWH()
 
     # 得到主图宽高
     h, w = mainimg.shape[:2]
 
     # 根据角度 随机获取调整仿射
-    d, x, y = get_random_affine_offset(angle_type, rw, rh)  # 类型 0上 1左 2下 3右 , x, y
+    d, x, y = getRandomAffineOffset(angleType, rw, rh)  # 类型 0上 1左 2下 3右 , x, y
 
     pts1 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
     pts2 = np.float32([[0, 0], [w, 0], [0, h], [w, h]])
@@ -239,14 +247,17 @@ def gen_affine_img(bgimg, mainimg, angle_type, label_file, fileName=None):
     print("调整仿射用时:", (datetime.datetime.now() - startTime).seconds)
 
     startTime = datetime.datetime.now()
-    # 将背景图大小按主图比例重置，宽度和高度随机大于主图
-    bgimg = cv.resize(bgimg, (w + rw * 2, h + rh * 2))
+    # 将背景图大小按主图随机扩大边框重置，宽度和高度随机大于主图
+    rdrw, rdrh = random.randint(0, rw), random.randint(0, rh)
+    rdrw = rdrw * random.randint(1, 2)
+    rdrh = rdrh * random.randint(1, 2)
+    bgimg = cv.resize(bgimg, (w + rdrw, h + rdrh))
 
     # 得到仿射后的图片宽高
     rows, cols, channels = mainimg.shape
 
     # 获取放置图片随机位置
-    ranrow, ranclo = rh, rw
+    ranrow, ranclo = rdrh, rdrw
 
     print("ranrow, ranclo", ranrow, ranclo)
 
@@ -275,18 +286,18 @@ def gen_affine_img(bgimg, mainimg, angle_type, label_file, fileName=None):
     # 将合并后的结果赋值给原背景图指定位置（ranrow,ranclo）
     bgimg[ranrow:rows + ranrow, ranclo:cols + ranclo] = dst
     # 返回图片或保存图片 名称格式：文件名_角度类型_?.png
-
-    (filepath, fileName) = os.path.split(fileName)
-    name, subfix = os.path.splitext(fileName)
-    outfile = OUT + name + "_" + str(angle_type) + ".png"
-    print("保存图片", outfile, angle_type)
+    (_, fileName) = os.path.split(fileName)
+    name, _ = os.path.splitext(fileName)
+    fileName = batchNum + "_" + name + "_" + str(angleType) + ".png"
+    outfile = OUT + fileName
+    print("保存图片", outfile, angleType)
     cv.imwrite(outfile, bgimg)
 
-    label_file.write("data/train/" + name + "_" + str(angle_type) + ".png")
-    label_file.write(" ")
-    label_file.write(str(angle_type))
-    label_file.write("\n")
-    print("合并保存图片用时:", (datetime.datetime.now() - startTime).seconds)
+    labelFile.write("data/train/" + fileName)
+    labelFile.write(" ")
+    labelFile.write(str(angleType))
+    labelFile.write("\n")
+    print("合并保存图片用时:", (datetime.datetime.now() - startTime).seconds, "\n")
 
 
 if __name__ == '__main__':
@@ -308,16 +319,28 @@ if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dir")  # 数据存放目录
-    parser.add_argument("--sum")  # 处理主图像的数量
+    parser.add_argument("--dir", nargs='?', const="", type=str)  # 数据存放目录
+    parser.add_argument("--sum", nargs='?', const=1000, type=int)  # 处理主图像的数量
+    parser.add_argument("--repeat", nargs='?', const=1, type=int)  # 重复执行次数
     args = parser.parse_args()
+
+    # args.sum = 100
+    # args.dir = "/Users/admin/local/creditease/ai/demo03/data/"
+
+    print("args = 【", args, "】")
+
+    if args.dir == "":
+        print("数据存放目录不正确")
+        exit()
+
     sum = int(args.sum)
+    repeat = int(args.repeat)
     ROOT = args.dir
     BACKGB = ROOT + "background/"
     IMAGES = ROOT + "origin/"
     OUT = ROOT + "second/"
     # 预先加载所有的纸张背景
-    all_bg_images = load_all_backgroud_images(BACKGB)
+    all_bg_images = loadAllBackgroudImages(BACKGB)
 
     # 循环1000随机加载主图片
     mainimg_list = os.listdir(IMAGES)
@@ -338,16 +361,18 @@ if __name__ == '__main__':
     milen = len(mainimg_list)
     label_file_name = os.path.join(ROOT, "second.txt")
     label_file = open(label_file_name, "w")
-    print("主图像总共数量", milen)
+
+    batchNum = datetime.datetime.now().strftime("batch-%H%M%S").strip()
+    print("主图像总共数量", milen, " 批次：", batchNum)
     if milen > sum:
         milen = sum
 
-    for idx in range(0, milen):
-        fileName = mainimg_list[idx]
-        mainimg = cv.imread(IMAGES + fileName)
-        # mainimg = Image.open(IMAGES + fileName)
-        gen_rotate_img(mainimg, fileName, label_file)
+    for maxIdx in range(0, repeat):
+        for idx in range(0, milen):
+            fileName = mainimg_list[idx]
+            mainimg = cv.imread(IMAGES + fileName)
+            genRotateImg(mainimg, label_file, fileName, batchNum)
 
     label_file.close()
 
-    print("总共用时", (datetime.datetime.now() - startTime).seconds)
+    print("总共用时:", (datetime.datetime.now() - startTime).seconds, " 批次：", batchNum)
