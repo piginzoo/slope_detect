b6ed4c88cf2cd9a1ea11496852eaa7a26268c0e8
hongyanyang1 <hongyanyang1@creditease.cn>
2019-08-07 10:46:47 +0800

    修改主杆

--- bin/train.sh
+++ bin/train.sh
@@ -58,7 +58,7 @@ nohup python -m main.train \
     --save_checkpoint_steps=5000 \
     --decay_rate=0.5 \
     --lambda1=1000 \
-    --gpu=1 \
+    --gpu=0 \
     --debug=False \
     --logs_path=logs \
     --moving_average_decay=0.997 \
