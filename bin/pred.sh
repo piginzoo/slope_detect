2d86d1dd41044cc44bc409f8c0dcfa095f6299c9
MaYanMei <yanmeima3@creditease.cn>
2020-04-09 11:45:31 +0800

    bugfix

--- bin/pred.sh
+++ bin/pred.sh
@@ -15,4 +15,4 @@ python main/pred.py \
     --pred_dir=data/validate \
     --debug=True \
     --model_dir=model \
-    --model_file=ctpn-2019-05-07-14-19-35-201.ckpt
+    --model_file=ctpn-2020-04-07-18-02-28-801.ckpt
