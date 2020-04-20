953c3a607531ec0e4e5c0b15fa25762c2ab4d0e4
MaYanMei <yanmeima3@creditease.cn>
2020-04-20 18:56:47 +0800

    update

--- bin/pb_pred.sh
+++ bin/pb_pred.sh
@@ -2,8 +2,7 @@ if [ "$1" = "help" ]; then
     echo "pb_pred.sh 使用说明：
     --image_dir     被预测的图片目录
     --pred_dir      预测后的结果的输出目录
-    --model_dir     model的存放目录，会自动加载最新的那个模型
-    --model_file    model的模型文件 "
+    --model_path     model的存放目录，会自动加载最新的那个模型
     exit
 fi
 
@@ -14,4 +13,4 @@ python pb_pred.py \
     --image_name=$1 \
     --pred_dir=data/validate \
     --debug=True \
-    --model_dir=model/pb/100000
+    --model_path=model/pb/100000
