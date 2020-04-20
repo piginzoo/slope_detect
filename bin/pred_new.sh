5154a908abc02bd1d5469fc153a2e4b5bac1f992
MaYanMei <yanmeima3@creditease.cn>
2020-04-20 13:23:53 +0800

    update

--- bin/pred_new.sh
+++ bin/pred_new.sh
@@ -1,5 +1,5 @@
 if [ "$1" = "help" ]; then
-    echo "pred.sh 使用说明：
+    echo "pred_new.sh 使用说明：
     --image_dir     被预测的图片目录
     --pred_dir      预测后的结果的输出目录
     --model_dir     model的存放目录，会自动加载最新的那个模型
@@ -9,7 +9,7 @@ fi
 
 echo "开始检测图片的倾斜....."
 
-python main/pred.py \
+python main/pred_new.py \
     --gpu=1 \
     --image_name=$1 \
     --pred_dir=data/validate \
