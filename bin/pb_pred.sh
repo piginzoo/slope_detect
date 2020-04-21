5a037a332fd9ba13552a1265cde98fda177c3d2c
MaYanMei <yanmeima3@creditease.cn>
2020-04-21 10:43:59 +0800

    bugfix

--- bin/pb_pred.sh
+++ bin/pb_pred.sh
@@ -2,7 +2,7 @@ if [ "$1" = "help" ]; then
     echo "pb_pred.sh 使用说明：
     --image_dir     被预测的图片目录
     --pred_dir      预测后的结果的输出目录
-    --model_path     model的存放目录，会自动加载最新的那个模型
+    --model_path     model的存放目录，会自动加载最新的那个模型 "
     exit
 fi
 
