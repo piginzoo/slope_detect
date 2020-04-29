abf594c5452e48c878a08a47140eb0c0b2b87a7c
MaYanMei <yanmeima3@creditease.cn>
2020-04-29 21:12:23 +0800

    update

--- main/train.py
+++ main/train.py
@@ -205,12 +205,15 @@ def main(argv=None):
             logger.info("结束第%d步训练，结束sess.run",step)
             # logger.info("结束第%d步训练，结果%r",classes)
 
-            # if step == 0:
-            #     sess.run([tf.assign(v_tr_text, tf.convert_to_tensor(str(pred_class)))])
-            #     sess.run([tf.assign(v_ori_text, tf.convert_to_tensor(str(label_list)))])
-            #     summary_writer.add_summary(summary_str, global_step=step)
+            sess.run(v_tr_text)
+            sess.run(v_ori_text)
 
-            if step == 0 and step % FLAGS.evaluate_steps == 0:
+            if step == 0:
+                sess.run([tf.assign(v_tr_text, tf.convert_to_tensor(str(pred_class)))])
+                sess.run([tf.assign(v_ori_text, tf.convert_to_tensor(str(label_list)))])
+                summary_writer.add_summary(summary_str, global_step=step)
+
+            if step != 0 and step % FLAGS.evaluate_steps == 0:
                 logger.info("在第%d步，开始进行模型评估",step)
                 sess.run([tf.assign(v_tr_text, tf.convert_to_tensor(str(pred_class)))])
                 sess.run([tf.assign(v_ori_text, tf.convert_to_tensor(str(label_list)))])
