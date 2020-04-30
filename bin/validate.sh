dd0ea53f337370c2a8d7d4d3832262667ed202de
MaYanMei <yanmeima3@creditease.cn>
2020-04-30 17:25:05 +0800

    update

--- bin/validate.sh
+++ bin/validate.sh
@@ -4,7 +4,6 @@ Date=$(date +%Y%m%d%H%M)
 
 python -m main.validate \
     --validate_dir=data/validate \
-    --validate_batch=1 \
     --validate_times=100 \
     --validate_label=data/validate.txt \
     --gpu=0 \
